05d0d151dab393eedec5765c09afff4855bbcb50
Ylan <ylan_vinicio@hotmail.com>
2023-06-30 21:53:58 -0300

    Countdown concluido

--- app.js
+++ app.js
@@ -26,8 +26,14 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2020, 4, 24, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+// let futureDate = new Date(2023, 6, 30, 22, 0, 0);
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -51,6 +57,36 @@ function getRemainingTime() {
     //* 1m = 60s
     //* 1hr = 60min
     //* 1d = 24hr
+
+    //* valores em ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    //* Calculando todos os valores
+    let days = t / oneDay
+    days = Math.floor(days)
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+
+    function format(item) {
+        if(item < 10) {
+            return item = `0${item}`;
+        }
+        return item
+    }
+
+    //* Definindo os valores dos array
+    const values = [days, hours, minutes, seconds];
+    items.forEach(function(item, index) {
+        item.innerHTML = format(values[index]);
+    })
+    if(t < 0){
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`
+    }
 }
+let countdown = setInterval(getRemainingTime, 1000);
 
 getRemainingTime();
